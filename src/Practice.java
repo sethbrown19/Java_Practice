7df04b6a07ab8291325fc2938ae85bf60f741369
Seth Alexander Brown <sethbrown19@outlook.com>
2020-12-14 18:41:33 -0500

    learned scanning inputs in and first if else statement used.

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,7 @@
+import java.nio.file.Path;
 import java.util.*;
 /** 
- * This program demonstrates console input
+ * 
  * @author sethb
  * @version 1.10 2020-12-14 
  */
@@ -9,19 +10,26 @@ public class Practice
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter your salary: ");
+		double yourSalary = in.nextDouble();
+		System.out.print("Please enter your sales in whole dollars: ");
+		double yourSales = in.nextDouble();
 		
-		// get first input
-		System.out.print("What is your name?");
-		String name = in.nextLine();
+		double target = 5000;
+		if (yourSales >= target)
+		{
+			String performance = "Satisfactory";
+			double bonus = 100 + (0.01 * (yourSalary - target));
+			double newSalary = yourSalary + bonus;
+			System.out.println("You have passed your target and performance is " + performance + ". Your new salary is " + newSalary + ".");
+		}
+		else 
+		{
+			String performance = "Unsatisfactory";
+			double bonus = 0;
+			System.out.println("You have not met your target and performance is " + performance + ". Your salary remains the same.");
+		}
 		
-		// get second input
-		System.out.print("How old are you?");
-		int age = in.nextInt();
-		
-		// display output  on console
-		System.out.println("Hello, " + name + ". Next year, you'll be " + (age +1));
 	}
 	
-	
-
 }
