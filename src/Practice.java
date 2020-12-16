c37e846184b58cc4d259ed9be1035afca92b4f64
Seth Alexander Brown <sethbrown19@outlook.com>
2020-12-15 20:42:11 -0500

    practicing arrays

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,4 @@
-import java.nio.file.Path;
-import java.util.*;
+
 /** 
  * 
  * @author sethb
@@ -9,27 +8,7 @@ public class Practice
 {
 	public static void main(String[] args)
 	{
-		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter your salary: ");
-		double yourSalary = in.nextDouble();
-		System.out.print("Please enter your sales in whole dollars: ");
-		double yourSales = in.nextDouble();
-		
-		double target = 5000;
-		if (yourSales >= target)
-		{
-			String performance = "Satisfactory";
-			double bonus = 100 + (0.01 * (yourSalary - target));
-			double newSalary = yourSalary + bonus;
-			System.out.println("You have passed your target and performance is " + performance + ". Your new salary is " + newSalary + ".");
-		}
-		else 
-		{
-			String performance = "Unsatisfactory";
-			double bonus = 0;
-			System.out.println("You have not met your target and performance is " + performance + ". Your salary remains the same.");
-		}
-		
+		int[] smallPrimes = {2, 3, 5, 7, 11, 13};
+		System.out.println(smallPrimes);
 	}
-	
 }
