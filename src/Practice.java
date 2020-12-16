f78c57f05cdd4f3833974ce4396f894559939834
Seth Alexander Brown <sethbrown19@outlook.com>
2020-12-16 12:32:00 -0500

    practicing arrays with for each loop and command line parameters.

--- src/Practice.java
+++ src/Practice.java
@@ -9,6 +9,6 @@ public class Practice
 	public static void main(String[] args)
 	{
 		int[] smallPrimes = {2, 3, 5, 7, 11, 13};
-		System.out.println(smallPrimes);
+		System.out.print(smallPrimes);
 	}
 }
